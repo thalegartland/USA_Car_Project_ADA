b829d4640158b3e675c84a443405335537c6ddc3
Thale Gartland <thale.gartland@gmail.com>
2022-10-28 13:48:12 +0200

    Updated distance sensor files to the correct ones

--- Project/src/distance_sensor.adb
+++ Project/src/distance_sensor.adb
@@ -1,10 +1,31 @@
 
 package body Distance_sensor is
+   
+   protected body Sensor_flag is
+      
+      procedure Set (Value : Boolean) is                                        --To set the value of Flag_value          
+      begin
+         
+         Flag_value := Value;
+         
+      end Set;
+      
+      function Get return Boolean is                                            --To return the value of Flag_value
+      begin
+         
+         return Flag_value;
+      
+      end Get;
+      
+   end Sensor_flag;
+         
 
    procedure Trigger (Trigger_pin_val : MicroBit.IOsForTasking.Pin_Id) is
 
-      Signal_duration : constant Ada.Real_Time.Time_Span := Ada.Real_Time.Microseconds(10);
-      Delay_time : Ada.Real_Time.Time := Ada.Real_Time.Clock + Signal_duration;
+      Signal_duration : constant Ada.Real_Time.Time_Span 
+        := Ada.Real_Time.Microseconds(10);
+      Delay_time : Ada.Real_Time.Time 
+        := Ada.Real_Time.Clock + Signal_duration;
       
    begin
       
@@ -23,30 +44,53 @@ package body Distance_sensor is
    begin
       MicroBit.IOsForTasking.Set(Echo_pin_val, True);
       
-      while MicroBit.IOsForTasking.Set(Echo_pin_val) = False loop
-         
+      while MicroBit.IOsForTasking.Set(Echo_pin_val) = False loop               --Updates Initial_time value until 
+                                                                                --confirmed that echo pin set to 1.
          Initial_time := Ada.Real_Time.Clock;
          
       end loop;
       
-      while MicroBit.IOsForTasking.Set(Echo_pin_val) loop
-         
-         Final_time := Ada.Real_Time.Clock;
-         
+      while MicroBit.IOsForTasking.Set(Echo_pin_val) loop                       --Updates Final_time value until
+                                                                                --signal confirmed returned, which
+         Final_time := Ada.Real_Time.Clock;                                     --is indicated by echo pin returning
+                                                                                --to 0.
       end loop;
       
-      Distance_detected := Float(To_Duration((34300*(Final_time - Initial_time))/2));
-      
+      Distance_detected := 
+        Float(To_Duration((34300*(Final_time - Initial_time))/2));              --Calculates distance travelled
+                                                                                --using time measured.
       return Distance_detected;
       
    end Echo;
    
-   function Measure_distance (Trigger_pin_val : MicroBit.IOsForTasking.Pin_Id; Echo_pin_val : MicroBit.IOsForTasking.Pin_Id) return Float is
+   
+   function Proximity_test return Boolean is
    begin
       
-      Trigger (Trigger_pin_val);
-      return Echo(Echo_pin_val);
+      if Echo(1) < 30.0 then
+         
+         return True;
+         
+      else
+         
+         return False;     
+         
+      end if;
+                    
+   end Proximity_test;
+   
+   
+   task body Sensor_loop is
       
-   end Measure_distance;
+   begin
+         
+      loop
+        
+         Trigger(0);
+         Sensor_flag.Set(Proximity_test);
+                  
+      end loop;
+      
+   end Sensor_loop;
    
 end Distance_sensor;
--- Project/src/distance_sensor.ads
+++ Project/src/distance_sensor.ads
@@ -1,11 +1,38 @@
 with MicroBit.IOsForTasking;
 with Ada.Real_Time; use Ada.Real_Time;
-with MicroBit.Console;
 
 package Distance_sensor is
 
-   procedure Trigger (Trigger_pin_val : MicroBit.IOsForTasking.Pin_Id);
-   function Echo (Echo_pin_val : MicroBit.IOsForTasking.Pin_Id) return Float;
-   function Measure_distance (Trigger_pin_val : MicroBit.IOsForTasking.Pin_Id; Echo_pin_val : MicroBit.IOsForTasking.Pin_Id) return Float;
+   protected Sensor_flag is                                                   --Declares protected object which
+                                                                              --encapsulates the flag value used to
+      procedure Set (Value : Boolean);                                        --indicate if the car is too close to
+      function Get return Boolean;                                            --an object.
 
+   private
+
+      Flag_value : Boolean := False;
+
+   end Sensor_flag;
+
+   procedure Trigger (Trigger_pin_val : MicroBit.IOsForTasking.Pin_Id);       --Sets trigger pin to 1 for 10
+                                                                              --microseconds to emit ultrasound
+                                                                              --signal.
+
+   function Echo (Echo_pin_val : MicroBit.IOsForTasking.Pin_Id) return Float; --Sets echo pin to 1 then monitors time
+                                                                              --taken for rebounded ultrasound signal
+                                                                              --to cause it to return to 0. The time
+                                                                              --taken for the signal to return is
+                                                                              --used to calculate the distance the
+                                                                              --signal travelled. The function  the
+                                                                              --returns calculated distance as a
+                                                                              --floating point value.
+
+   function Proximity_test return Boolean;                                    --Checks if the sensor detects an
+                                                                              --object that's too close (within 30cm)
+                                                                              --and, if so, returns True.
+
+   task Sensor_loop with Priority => 1;                                       --Declares a task to be used to loop
+                                                                              --the sensor functions so as to
+                                                                              --test continuously whether the car
+                                                                              --is too near another object.
 end Distance_sensor;
